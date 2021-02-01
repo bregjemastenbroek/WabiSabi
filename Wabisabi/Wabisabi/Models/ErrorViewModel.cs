53ea715b22d450599cdc9f357ba010eb1692b506
Bregje Mastenbroek <110273@st-maartenscollege.nl>
2021-02-01 14:40:37 +0100

    Initial

---
+++ Wabisabi/Wabisabi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Wabisabi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
