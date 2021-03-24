f5057837d4bdc6719efa5766449fbe656eedf2d6
KatexAllen <45490818+KatexAllen@users.noreply.github.com>
2021-03-24 15:42:39 +0100

    error gefixt

--- Wabisabi/Wabisabi/Controllers/HomeController.cs
+++ Wabisabi/Wabisabi/Controllers/HomeController.cs
@@ -74,7 +74,7 @@ namespace Wabisabi.Controllers
                     }
                 }
             }
-            return countries.First();
+            return countries.FirstOrDefault();
         }
 
 
